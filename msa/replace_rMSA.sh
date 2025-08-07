f97710e6b114c5466eaaca43724567f82ba04c81
elkerist <137736564+elkerist@users.noreply.github.com>
2025-08-08 02:20:21 +0800

    replace_rMSA.sh

--- msa/replace_rMSA.sh
+++ msa/replace_rMSA.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# 获取脚本所在目录的绝对路径
+# Get the absolute path of the directory where the script is located
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 PROJECT_ROOT="$(dirname "$SCRIPT_DIR")"
 
