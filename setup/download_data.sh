ea29035cc1442ba4b7e40115491c15f43f38a860
elkerist <137736564+elkerist@users.noreply.github.com>
2025-11-05 17:00:50 +0800

    Update download_data.sh

--- setup/download_data.sh
+++ setup/download_data.sh
@@ -5,9 +5,12 @@ DOWNLOAD_DIR="./data"
 mkdir -p $DOWNLOAD_DIR
 cd $DOWNLOAD_DIR
 
-# Download RNACentral data file
-echo "Downloading RNACentral data file..."
-wget -O rnacentral_active.fasta.gz https://ftp.ebi.ac.uk/pub/databases/RNAcentral/releases/24.0/sequences/rnacentral_active.fasta.gz
+# Download RNACentral data file (latest version)
+echo "Downloading RNACentral data file (latest version)..."
+wget -O rnacentral_active.fasta.gz https://ftp.ebi.ac.uk/pub/databases/RNAcentral/current_release/sequences/rnacentral_active.fasta.gz
+
+# If you want to reproduce the exact results from our paper, uncomment the line below and comment out the line above:
+# wget -O rnacentral_active.fasta.gz https://ftp.ebi.ac.uk/pub/databases/RNAcentral/releases/24.0/sequences/rnacentral_active.fasta.gz
 
 # Check if download was successful
 if [ $? -eq 0 ]; then
